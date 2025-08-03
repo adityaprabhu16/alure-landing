3273dd34c87d917beb2cde71bc6ef00d2e9ae4ba
Aditya Prabhu <90486414+adityaprabhu16@users.noreply.github.com>
2025-08-03 14:24:51 -0500

    readded

--- portfolio-app/src/routes/Demo.js
+++ portfolio-app/src/routes/Demo.js
@@ -4,18 +4,32 @@ import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 import HeroImg2 from '../components/HeroImg2.js';
 import Work from "../components/Work.js";
-import Info from "../components/Info.js";
 import DemoStyles from "../components/DemoStyles.css";
+import InfoStyles from "../components/InfoStyles.css";
 
 // Import video files
 import AlureDemo from '../assets/demos/AlureDemo.mov';
 import AlureRender from '../assets/demos/AlureRender.mov';
+import AlureProduct from "../assets/beetle_samples.png";
 
 const Demo = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="What's Alure?" text="What are we doing, and why is it important?" backgroundImage="../assets/backgrounds/project-bg.png"/>
+      
+      <div className="info-container">
+        <h1>Japanese Beetle Prevention, <span className="gradient-text">Reinvented.</span></h1>
+        <br />
+        <p>Japanese Beetles (above) are an invasive species to U.S. It is an agricultural and commercial pest that causes heavy plant damage, costing the average vineyard, orchard, or commercial grower over $1,000 an acre to manage. These beetles destroy over <span className="gradient-text">300 common plant species</span>, from soybeans to rosebushes. Unfortunately, the only way to manage them is through harmful pestcides that kill pollinators and sterilize the environment.</p>
+        <br />
+        <h1>Sustainable, Simple, Aesthetic.</h1>
+        <br />
+        <img src={AlureProduct} alt="Alure LLC bait and kill device"/>
+        <p>Alure LLC has developed a unique approach (above) to managing Japanese Beetles: Bait and Kill surfaces. Instead of pouring harmful pesticides on your plants several times each season, endangering both pollinators and the health of your produce, our unique surface <span className="gradient-text">baits the bugs and kills them on contact.</span> The product is safe for humans and pet, and we are working on EPA product registration to offer this solution commercially. No more refilling bags of dead beetles, or attracting more beetles from current traps. Our surface solution finishes the job by both baiting and killing.</p>
+      </div>
+
+      <div className="section-divider"></div>
       
       <div className="demo-section">
         <h1>Virtual Field Site Demo</h1>
@@ -57,9 +71,6 @@ const Demo = () => {
         </div>
       </div>
 
-      <div className="section-divider"></div>
-      
-      <Info /> 
       <Work />
       
       <Footer />
